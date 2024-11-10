aa7d40ae554173407f6b96a02e92e8519357cc3a
wallysonlima <wallyson.n.a.lima@gmail.com>
2024-11-10 11:25:20 -0300

    first commit

---
+++ websmartoffice/src/main/kotlin/lima/wallyson/WebSmartOfficeApplication.kt
@@ -0,0 +1,11 @@
+package lima.wallyson
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebSmartOfficeApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebSmartOfficeApplication>(*args)
+}
---
+++ websmartoffice/src/test/kotlin/lima/wallyson/WebSmartOfficeApplicationTests.kt
@@ -0,0 +1,13 @@
+package lima.wallyson
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebSmartOfficeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
