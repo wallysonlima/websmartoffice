a0963a05d16120a9b38a8bf749ee780f4e12b349
wallysonlima <wallyson.n.a.lima@gmail.com>
2025-03-25 22:07:02 -0300

    diversas alteracoes, quase pronto.

--- WebSmartOffice/src/main/kotlin/lima/wallyson/WebSmartOffice/infraestructure/database/repository/AddressRepository.kt
+++
@@ -1,6 +0,0 @@
-package lima.wallyson.WebSmartOffice.infraestructure.database.repository
-
-import lima.wallyson.WebSmartOffice.infraestructure.database.entity.AddressEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AddressRepository: JpaRepository<AddressEntity, Long> {}
